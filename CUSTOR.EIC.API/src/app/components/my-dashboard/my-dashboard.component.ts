ccf6c1297e115cae86cb3d750332fab9b5518643
addisalem tafere <addisalem12@gmail.com>
2018-12-24 22:36:28 +0300

    minor modification

--- CUSTOR.EIC.API/src/app/components/my-dashboard/my-dashboard.component.ts
+++ CUSTOR.EIC.API/src/app/components/my-dashboard/my-dashboard.component.ts
@@ -33,7 +33,7 @@ export class MyDashboardComponent implements OnInit, AfterViewInit {
   loading = true;
   searchForm: FormGroup;
   serviceList: ServiceModel[] = [];
-  displayedColumns = ['CaseNumber', 'ServiceName', 'ProjectName', 'currentStatus', 'NextStep', 'Action'];
+  displayedColumns = ['CaseNumber', 'ServiceName', 'currentStatus', 'NextStep', 'Action'];
   displayedColumnsNotification = ['subject', 'date', 'message', 'Action'];
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
