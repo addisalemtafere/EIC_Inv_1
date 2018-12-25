2b0426250a7485ad4c15b94bc49096c001cf5668
addisalem tafere <addisalem12@gmail.com>
2018-12-25 19:09:46 +0300

    minor modification

--- CUSTOR.EIC.API/src/app/Services/followup/followup.service.ts
+++ CUSTOR.EIC.API/src/app/Services/followup/followup.service.ts
@@ -1,14 +1,16 @@
-
 import {FollowUpModel} from "../../model/followup/followup.model";
 import {ErrorMessage} from "@custor/services/errMessageService";
 import {AppConfiguration} from "../../../../../CUSTOR.EIC.API/src/app/config/appconfig";
 import {BaseService} from "../../../../../CUSTOR.EIC.API/src/app/Services/Base.service";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
 })
-export class FollowupService extends BaseService<FollowUpModel>{
-followups:FollowUpModel;
+export class FollowupService extends BaseService<FollowUpModel> {
+  followups: FollowUpModel;
+
   constructor(
     protected http: HttpClient,
     protected appConfig: AppConfiguration,
