a3e679dc04e576233446ab67076e6a1c2198d28c
addisalem tafere <addisalem12@gmail.com>
2019-01-12 18:00:28 +0300

    minor Modification

--- CUSTOR.EIC.API/src/@custor/services/configuration.service.ts
+++ CUSTOR.EIC.API/src/@custor/services/configuration.service.ts
@@ -18,11 +18,11 @@ export class ConfigurationService {
   public static readonly appVersion: string = '1.0';
 
   // public baseUrl = environment.baseUrl || Utilities.baseUrl();
-  public baseUrl = 'http://localhost:5050/';
+  public baseUrl = 'http://eic.eastus.cloudapp.azure.com/';
   // public tokenUrl = environment.tokenUrl || environment.baseUrl || Utilities.baseUrl();
-  public tokenUrl = 'http://localhost:5050';
+  public tokenUrl = 'http://eic.eastus.cloudapp.azure.com';
   public loginUrl = environment.loginUrl;
-  public fallbackBaseUrl = 'http://localhost:5050';
+  public fallbackBaseUrl = 'http://eic.eastus.cloudapp.azure.com';
 
   public static readonly defaultLanguage: string = 'et';
   public static readonly defaultHomeUrl: string = '/';
--- CUSTOR.EIC.API/src/environments/environment.ts
+++ CUSTOR.EIC.API/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //baseUrl: 'http://172.17.33.132/',
-  baseUrl: 'http://localhost:5050/',
+  baseUrl: 'http://eic.eastus.cloudapp.azure.com/',
   tokenUrl: null, // Set to null if same as baseUrl
   loginUrl: '/Login'
 };
