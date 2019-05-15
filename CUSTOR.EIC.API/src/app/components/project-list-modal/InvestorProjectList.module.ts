43570cc8259510ee60f99a9bcd1c221a9a9cf2fb
addisalem tafere <addisalem12@gmail.com>
2019-05-15 16:52:36 +0300

    pagination added to service application

--- CUSTOR.EIC.API/src/app/components/project-list-modal/InvestorProjectList.module.ts
+++ CUSTOR.EIC.API/src/app/components/project-list-modal/InvestorProjectList.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {SharedModule} from '@custor/modules/shared.module';
 import {InvestorProjectListRoutingModule} from './investorProjectList-routing.module';
 import {ProjectListModalComponent} from './project-list-modal.component';
+import {PaginationService} from "@custor/services/pagination.service";
 
 @NgModule({
   imports: [
---
+++ CUSTOR.EICOnline.DAL/EntityLayer/Common/QueryParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CUSTOR.EICOnline.DAL.EntityLayer
+{
+    public class QueryParameters
+    {
+        public int PageNumber { get; set; } = 1;
+//        public int CustomerId { get; set; } = 1;
+        public int PageCount  { get; set; } = 10;
+        public string Lang { get; set; }
+    }
+}
